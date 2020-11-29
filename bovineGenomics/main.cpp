b4f962e951d8c3c769316ca305b2d1e269d0900f
anuva04 <57056078+anuva04@users.noreply.github.com>
2020-11-29 22:01:22 +0530

    USACO Bronze complete search

---
+++ bovineGenomics/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("cownomics.in", "r", stdin);
+    freopen("cownomics.out", "w", stdout);
+    int n, m; cin>>n>>m;
+    string spotty[n];
+    for(int i=0; i<n; i++){
+        cin>>spotty[i];
+    }
+    string plain[n];
+    for(int i=0; i<n; i++){
+        cin>>plain[i];
+    }
+    int ans = 0;
+    for(int i=0; i<m; i++){
+        set<char> s;
+        for(int j=0; j<n; j++){
+            s.insert(spotty[j][i]);
+        }
+        bool isPresent = false;
+        for(int j=0; j<n; j++){
+            if(s.find(plain[j][i])!=s.end()){
+                isPresent=true;
+                break;
+            }
+        }
+        if(isPresent==false) ans++;
+    }
+    cout << ans;
+    return 0;
+}
---
+++ cowGymnastics/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int arr[10][20];
+int solve(int a, int b){
+    int ans = 0;
+    for(int session=0; session<k; session++){
+        int apos, bpos;
+        for(int i=0; i<n; i++){
+
+            if(arr[session][i]==a) apos = i;
+            if(arr[session][i]==b) bpos = i;
+        }
+        if(apos<bpos) ans++;
+    }
+    return ans;
+}
+int main()
+{
+    freopen("gymnastics.in", "r", stdin);
+    freopen("gymnastics.out", "w", stdout);
+    cin>>k>>n;
+
+    for(int i=0; i<k; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    int res=0;
+    for(int a=1; a<=n; a++){
+        for(int b=1; b<=n; b++){
+            if(solve(a,b)==k){
+                res++;
+            }
+        }
+    }
+    cout << res;
+    return 0;
+}
