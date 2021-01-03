ef0d6fff9c3a42f457ed7c1ac03a244cc923871a
Anuva Bhattacharjee <57056078+anuva04@users.noreply.github.com>
2021-01-04 00:38:55 +0530

    Update main.c

--- lift/main.c
+++ lift/main.c
@@ -1,17 +1,33 @@
-#include <stdio.h>
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/lift-queries/description/
 
-int main(){
-    int T, N, a, b;
-    scanf("%d", &T);
-    for(int i=0; i<T; i++){
-        scanf("%d", &N);
-        a = N;
-        b = 7-N;
-        if(a<=b){
-            printf("A\n");
-        }
-        else {
-            printf("B\n");
-        }
-    }
+#include <stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+int i,j,n;
+scanf("%d",&n);
+int a=0,b=7,c=0,d=0;
+for(i=0;i<n;i++)
+{
+scanf("%d",&j);
+c=abs(a-j);
+d=abs(b-j);
+if(c<d)
+{
+printf("A\n");
+a=j;
+}
+else if(c>d)
+{
+printf("B\n");
+b=j;
+}
+else
+{
+printf("A\n");
+a=j;
+}
+}
+return 0;
 }
